2015f0707ce4fd2e2e54420f2caa7def0ffbf44b
Elliott Willson <elliottwillson@gmail.com>
2020-11-28 01:11:41 -0500

    added html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //test to see what data is returned from API call
 const testData = async(cityName, stateCode, countryCode) => {
     
-    const getTestData = await fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}, ${countryCode}&units=imperial&appid=39fc93c2a4e618396ba3aa95200af967`);
+    const getTestData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}, ${countryCode}&units=imperial&appid=39fc93c2a4e618396ba3aa95200af967`);
     
     const jsonGetTestData = await getTestData.json();
     console.log(getTestData);
